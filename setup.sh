6739d1163ba5fde1afc5beba06371a9579aaf846
Colby <60820431+ComputerTech312@users.noreply.github.com>
2022-06-24 07:53:31 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,14 @@
 sudo -i
-
 apt update
-
 apt upgrade -y
-
 apt-get install build-essential pkg-config libssl-dev libpcre2-dev libargon2-dev libsodium-dev libc-ares-dev libcurl4-openssl-dev net-tools fail2ban apache2 curl wget zip unzip htop
-
 ufw --force disable
-
 ufw --force reset
-
 ufw logging on
-
 ufw default deny incoming
-
 ufw default allow outgoing
-
-
 ufw allow ssh
-
 ufw -- force enable
-
 service ssh restart
-
 reboot
 
